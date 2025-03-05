4781f385896009d0164be215503dc66380f6d0f6
thinh15v <thinh15v@gmail.com>
2025-03-05 08:50:56 +0700

    lab 2

---
+++ lab-2.py
@@ -0,0 +1,11 @@
+#Viết chương trình để đảo ngược vị trí của các phần tử trong một danh sách. 
+def dao_nguoc_list(lst):
+    return lst[::-1]
+
+#Nhập danh sách từ người dùng và xử lý chuỗi 
+input_list = input("Nhập danh sách các số , cách nhau bởi dấu phẩy:")
+numbers = list(map(int,input_list.split(',')))
+
+#Sử dụng hàm và in kết quả
+list_dao_nguoc = dao_nguoc_list(numbers)
+print("List sau khi đảo ngược:",list_dao_nguoc)
